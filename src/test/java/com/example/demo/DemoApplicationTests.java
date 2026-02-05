c3adf5014cc147285e7ad143b6fc965cc4cb2d0c
ernest-emmanuel-utibe <em123nest@gmail.com>
2026-02-05 21:15:00 +0100

    Sixth of Something

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,7 +3,11 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+  properties = {
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+  }
+)
 class DemoApplicationTests {
 
 	@Test
